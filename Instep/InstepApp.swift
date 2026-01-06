05782f3e559c0a54d50d9e843fe0a8f3326b52ba
jitbon <pauljhwoo@gmail.com>
2026-01-05 22:07:51 -0500

    new initialization

---
+++ Instep/InstepApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstepApp.swift
+//  Instep
+//
+//  Created by Paul Woo on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct InstepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstepUITests/InstepUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  InstepUITestsLaunchTests.swift
+//  InstepUITests
+//
+//  Created by Paul Woo on 1/5/26.
+//
+
+import XCTest
+
+final class InstepUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
